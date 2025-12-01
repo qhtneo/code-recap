40862bcbebeca08b61ffca9b35a97d35829dc13e
qhtneo <hj9671@gmail.com>
2025-12-01 22:09:25 +0900

    feat(exception):add PostNotFoundException, GlobalExceptionHandler

---
+++ src/main/java/orderhub/coderecap/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package orderhub.coderecap.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handlePostNotFFound(PostNotFoundException ex) {
+        Map<String, Object> body = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.NOT_FOUND.value(),
+                "error", "Not Found",
+                "message", ex.getMessage()
+        );
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
